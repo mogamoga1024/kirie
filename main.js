cbf6def9fcc0ecee181d66c7289119f5bc46b866
mogamoga <mogamoga.g1thu6@gmail.com>
2024-06-13 13:41:06 +0900

    わんだほーい

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+let isLoadingInputImage = false;
+
 const App = {
     data() {
         return {
@@ -10,7 +12,31 @@ const App = {
     },
     methods: {
         onChangeInputImageFile(e) {
-            // noop
+            if (isLoadingInputImage) {
+                return;
+            }
+            isLoadingInputImage = true;
+
+            const imageFile = e.target.files[0];
+            e.target.value = "";
+
+            if (!imageFile.type.startsWith("image")) {
+                alert("画像ファイルを選択してください。");
+                isLoadingInputImage = false;
+                return;
+            }
+
+            const image = new Image();
+            image.onload = () => {
+                this.imageFileName = imageFile.name;
+                isLoadingInputImage = false;
+            };
+            image.onerror = () => {
+                alert("画像の読み込みに失敗しました。");
+                isLoadingInputImage = false;
+            };
+
+            image.src = URL.createObjectURL(imageFile);
         },
     }
 };
