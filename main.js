9e0126b894b169e29f5a66f20c0ee073406700e6
mogamoga <mogamoga.g1thu6@gmail.com>
2024-06-13 10:58:53 +0900

    臆病もバイバイする

---
+++ main.js
@@ -0,0 +1,16 @@
+
+const App = {
+    data() {
+        return {
+
+        };
+    },
+    created() {
+        // noop
+    },
+    methods: {
+        // noop
+    }
+};
+
+Vue.createApp(App).mount("#app");
